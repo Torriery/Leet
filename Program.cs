fd52b51c9c0d0002a2e5f35ff3470c5865dd13a9
torriery <andreastorrieri3@gmail.com>
2022-03-25 21:35:03 -0300

    finalizar

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Write("texto a ser traduzido: ");
+Console.WriteLine(
+Console.ReadLine()!
+.Replace("a","4")
+.Replace("A","4")
+.Replace("e","3")
+.Replace("E","3")
+.Replace("i","1")
+.Replace("I","1")
+.Replace("l","1")
+.Replace("L","1")
+.Replace("o","0")
+.Replace("O","0")
+.Replace("t","7")
+.Replace("T","7")
+.Replace("s","5")
+.Replace("S","5")
+);
